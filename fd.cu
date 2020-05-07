448adef93446b59845475027cb51c3f6fcf697a8
AmirulOm <amirul.abdullah89@gmail.com>
2020-05-07 11:56:21 +0800

    final optimization

--- fd.cu
+++ fd.cu
@@ -2,11 +2,19 @@
 #include <iostream>
 #include <array>
 #include <assert.h>
+#include <math.h> 
 
 #ifndef SIZE
 #define SIZE 10000
 #endif
 
+#ifndef BLOCK_SIZE
+#define BLOCK_SIZE 512
+#endif
+
+#define STENCIL_SIZE 3
+
+// Cuda error checker
 inline
 cudaError_t checkCuda(cudaError_t result)
 {
@@ -21,7 +29,8 @@ cudaError_t checkCuda(cudaError_t result)
 
 __global__ void fd_kernel(double *a, double *b)
 {
-    __shared__ double df[3][1000];
+    // Reusable shared memory
+    __shared__ double df[STENCIL_SIZE][BLOCK_SIZE];
   
     const int i = threadIdx.x;
     const int offset = blockIdx.x;
@@ -29,11 +38,13 @@ __global__ void fd_kernel(double *a, double *b)
     const size_t rowOffset = SIZE; 
     int lastThread = blockDim.x - 1;
     size_t row=0;
-
+    
+    //Last block checker to disable thread overflow
     if( blockIdx.x == (gridDim.x -1) ){
       lastThread = rowOffset - ((blockIdx.x  * blockDim.x) - offset) - 1;
     }
     
+    //copy first two row to shared memory from global
      if( globalInd < rowOffset ){
       df[0][i] = a[globalInd + (rowOffset * row)];
       df[1][i] = a[globalInd + (rowOffset * (row+1))] ;
@@ -41,9 +52,11 @@ __global__ void fd_kernel(double *a, double *b)
   
       for(; row < SIZE-2; ++row)
       {
-         if( globalInd < rowOffset ){
+        //copy third two row to shared memory from global
+        if( globalInd < rowOffset ){
           df[2][i] = a[globalInd + (rowOffset * (row+2))];
-         }
+        }
+
         __syncthreads();
   
         if( globalInd < rowOffset ){
@@ -53,13 +66,12 @@ __global__ void fd_kernel(double *a, double *b)
           {  
             b[id]= df[1][i]/2.0 + df[1][i+1]/8.0 + df[1][i-1]/8.0 +  df[0][i]/8.0 +  df[2][i]/8.0;
           }
-            
+          
+          //move shared memory to load the next row.
           df[0][i] = df[1][i];
           df[1][i] = df[2][i];
         }
       }
-     
-   
 }
 
 
@@ -93,10 +105,10 @@ void fd()
     checkCuda( cudaMemcpy(a_dev, a_host, sizeof(double) * SIZE * SIZE, cudaMemcpyHostToDevice) );  
     checkCuda( cudaMemcpy(b_dev, a_host, sizeof(double) * SIZE * SIZE, cudaMemcpyHostToDevice) ); 
 
-    dim3 grid(11), block(1000);
+    dim3 grid( ceil(SIZE/(float)(BLOCK_SIZE-STENCIL_SIZE+1)) ), block(BLOCK_SIZE);
     
     for (size_t iter = 0; iter < ITER_SIZE; ++iter)
-      if( iter % 2 == 0)
+      if( iter % 2 == 0) // switcher to avoid memory copy between a_dev and b_dev
         fd_kernel<<<grid,block>>>(a_dev,b_dev);
       else
         fd_kernel<<<grid,block>>>(b_dev,a_dev);
@@ -118,8 +130,6 @@ int main()
     checkCuda( cudaGetDeviceProperties(&prop, 0) );
     std::cout << "\nDevice Name: \n" << prop.name;
     std::cout << "Compute Capability: " << prop.major << "." << prop.minor << "\n";
-    std::cout << "maxThreadsDim    : " << prop.maxThreadsDim[0] << "," << prop.maxThreadsDim[1] << "," << prop.maxThreadsDim[2] << "\n";
-    std::cout << "maxThreadsPerBlock: " << prop.maxThreadsPerBlock << "\n";
 
     fd();
 
